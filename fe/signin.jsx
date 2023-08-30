3f66f4c09bb31d528995a8a0bcc3e69beac1b78a
Arjith_arch <arjtihh7@gmail.com>
2023-08-31 00:33:31 +0530

    wip signin page

--- fe/signin.jsx
+++ fe/signin.jsx
@@ -1,48 +1,46 @@
-import React from "react";
-import Button from 'react-bootstrap/Button';
-import Form from 'react-bootstrap/Form';
-import { Nav_bar} from "./home.jsx";
-export function Signin(){
-    return(
+import React, { useState } from "react";
+import { Nav_bar } from "./home.jsx";
+export function Signin() {
+    const [b1,setb1]=useState("1px solid")
+    const [b2,setb2]=useState("none")
+    function change(){
+
+    }
+
+    return (
         <>
-        <div style={{overflow:"hidden"}}>
-            
-        <Nav_bar  phone={false}shoe={false} tshirt={false} home={true} widths={"1900px"}/>
-       <div className="sign_cont">
-        <div className="form">
-            <div className="tabs">
-                <table>
-                    <tbody>
-                        <tr>
-                            <td>Sign in</td>
-                            <td>Register</td>
-                        </tr>
-                    </tbody>
-                </table>
-            </div>
-            <div style={{padding:"100px"}}>
+            <div style={{ overflow: "hidden" }}>
 
-            <Form>
-      <Form.Group className="mb-3 sm2" controlId="formBasicEmail">
-        <Form.Label>Email address</Form.Label>
-        <Form.Control type="email" placeholder="Enter email" />
-     
-      </Form.Group>
+                <Nav_bar phone={false} shoe={false} tshirt={false} home={true} widths={"1900px"} />
+                <div className="sign_cont">
+                    <div className="form">
+                        <div className="tabs">
+                            <table>
+                                <tbody>
+                                    <tr>
+                                        <td style={{borderRight:b1}} onClick={change}>Sign in</td>
+                                        <td style={{borderBottom:b2}} onClick={change}>Register</td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                        </div>
+                        <div className="inputs">
+                      
+                                <label htmlFor="email" style={{margin:"20px"}}>Username
+                                <input type="text" style={{margin:"10px",width:"300px",borderRadius:"10px",border:"1px solid grey",padding:"5px"}} name="email" placeholder="john@gmail.com" />
+                                </label>
 
-      <Form.Group className="mb-3" controlId="formBasicPassword">
-        <Form.Label>Password</Form.Label>
-        <Form.Control type="password" placeholder="Password" />
-      </Form.Group>
-   
-      <Button variant="primary" type="submit">
-        Submit
-      </Button>
-    </Form>
+                                <label htmlFor="pass" style={{margin:"20px"}}>Password
+                                <input type="password" name="pass" id="" style={{margin:"10px",width:"300px",borderRadius:"10px",border:"1px solid grey",padding:"5px"}} />
+                                </label>
+                                <button style={{width:"120px",border:"1px solid black"}}>Sign in</button>
+
+
+                        </div>
+
+                    </div>
+                </div>
             </div>
-          
-        </div>
-       </div>
-        </div>
         </>
     )
 
